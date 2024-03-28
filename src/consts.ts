4a43460aa8880df0a6aeef7e37b4e628b70d1143
jamiepepper <jamie@pepper.digital>
2024-03-28 14:14:59 +0000

    built Every Other Friday landing page

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Every Other Friday - Business ideas to move the world';
+export const SITE_DESCRIPTION = 'Join our newsletter to get the latest business ideas and insights from Every Other Friday.';
